7f3e68bbdde68c262577691e69e36137b7d652cf
Andrew Dickinson <fortknoxcomputer@gmail.com>
2014-10-31 23:37:59 -0400

    Clean up managealliance template/add total points

--- points.py
+++ points.py
@@ -1,4 +1,5 @@
 import logging
+from alliance_management import get_team_schedule
 from datastore_classes import Team_Event, team_event_key, team_key
 from progress_through_elimination_classification import UNDETERMINED, DIDNTQUALIFY, QUARTERFINALIST, SEMIFINALIST, FINALIST, WINNER
 """Uses point system described in http://www.chiefdelphi.com/media/papers/2574"""
@@ -38,21 +39,34 @@ def get_seed_points(seed):
 
 def get_team_points_at_event(team_number, event_id):
     team_event = Team_Event.get_or_insert(team_event_key(team_key(str(team_number)), event_id).id(), parent=team_key(str(team_number)))
-    qual_win_points = team_event.win * points_per_qual_win
-    qual_tie_points = team_event.tie * points_per_qual_tie
-    qual_loss_points = team_event.loss * points_per_qual_loss
-    elimination_points = elemination_progress_points[team_event.elimination_progress]
-    seed_points = get_seed_points(team_event.rank)
-    award_points = 0
-    for award in team_event.awards:
-        award_points += award_points_by_TBA_id[award]
-    total_points = ( qual_win_points +
-                     qual_tie_points +
-                     qual_loss_points +
-                    elimination_points +
-                     seed_points +
-                     award_points)
+    if team_event.win: #For debug purposes, so unloaded events don't raise exceptions
+        qual_win_points = team_event.win * points_per_qual_win
+        qual_tie_points = team_event.tie * points_per_qual_tie
+        qual_loss_points = team_event.loss * points_per_qual_loss
+        elimination_points = elemination_progress_points[team_event.elimination_progress]
+        seed_points = get_seed_points(team_event.rank)
+        award_points = 0
+        for award in team_event.awards:
+            award_points += award_points_by_TBA_id[award]
+        total_points = ( qual_win_points +
+                         qual_tie_points +
+                         qual_loss_points +
+                        elimination_points +
+                         seed_points +
+                         award_points)
+    else:
+        total_points = 0
     return total_points
+
+def get_points_to_date(team_number):
+    schedule = get_team_schedule(team_number)
+    points = 0
+    for week, event in enumerate(schedule):
+        if schedule[week]['competition_name'] != "":
+
+            event_key = schedule[week]['event_key']
+            points = points + get_team_points_at_event(team_number, event_key)
+    return points
 
 
 #Uses ids based off of https://github.com/the-blue-alliance/the-blue-alliance/blob/master/consts/award_type.py
