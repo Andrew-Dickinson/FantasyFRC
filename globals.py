992038096330717024e58264b765464fa6db3462
Andrew Dickinson <fortknoxcomputer@gmail.com>
2015-02-21 00:31:47 -0500

    Made limit reasonable

--- globals.py
+++ globals.py
@@ -16,7 +16,7 @@ draft_time_minutes = 2  # Time per person per round
 number_of_locked_teams = 5  # Number of non-transferable teams to have at the top of the rankings
 
 maximum_roster_size = 30  # The maximum number of teams a player can have on their roster
-maximum_active_teams = 2  # The maximum number of teams a player can have active at once
+maximum_active_teams = 6  # The maximum number of teams a player can have active at once
 
 number_of_official_weeks = 7  # Used in general to iterate over schedule
 number_of_round_robin_weeks = 7  # Used to create the schedule
